096e0534770fe5fa086ffcbbca830d899692b616
negan947 <alexvb1@protonmail.com>
2023-07-20 17:35:20 +0300

    phone instead of fb

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,6 +8,7 @@ import { HashLink } from 'react-router-hash-link';
 import {
   BrowserRouter as Router
 } from "react-router-dom";
+import { FaPhone } from "react-icons/fa";
 
 export const NavBar = () => {
 
@@ -59,7 +60,7 @@ export const NavBar = () => {
             <span className="navbar-text">
               <div className="social-icon">
                 <a href="#"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
+                <Nav.Link href="tel:1234567890"><FaPhone style={{color: 'white'}} /></Nav.Link>
                 <a href="#"><img src={navIcon3} alt="" /></a>
               </div>
               <HashLink to='#connect'>
