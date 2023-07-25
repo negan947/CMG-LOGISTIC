f8615690f9a5a16eae37784e8641d026f8920848
negan947 <alexvb1@protonmail.com>
2023-07-25 12:47:51 +0300

    in progress testimonials

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,66 +1,88 @@
 import React from 'react';
 
 export const Testimonials = () => {
-  const testimonials = [
-    {
-      id: 1,
-      image: 'testimony1.jpg', // Replace with the image path for the first testimonial
-      quote: 'Testimonial 1 text',
-    },
-    {
-      id: 2,
-      image: 'testimony2.jpg', // Replace with the image path for the second testimonial
-      quote: 'Testimonial 2 text',
-    },
-    {
-      id: 3,
-      image: 'testimony3.jpg', // Replace with the image path for the third testimonial
-      quote: 'Testimonial 3 text',
-    },
-    // Add more testimonials if needed
-  ];
-
   return (
-    <div style={{ background: 'linear-gradient(to right, #3498db, #9b59b6)', padding: '20px' }}>
-      <div style={{ textAlign: 'center' }}>
-        <h2>Testimonials</h2>
-        <p>Short description about testimonials.</p>
+    <div style={{ 
+      display: 'flex',
+      justifyContent: 'center',
+      gap: '20px',
+      flexWrap: 'wrap',
+      flexDirection: 'row',
+      '@media (max-width: 600px)': {
+        flexDirection: 'column',
+      },
+      marginTop: '40px',
+      marginBottom: '40px',
+    }}>
+      <div style={{ 
+        width: '100%',
+        textAlign: 'center',
+        fontSize: '24px',
+        fontWeight: 'bold',
+        marginBottom: '20px',
+      }}>
+        Testimonials
       </div>
-
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <div
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-            width: '70%',
-            margin: '0 auto',
-            position: 'relative',
-          }}
-        >
-          {testimonials.map((testimonial) => (
-            <div
-              key={testimonial.id}
-              style={{
-                width: '30%',
-                background: 'rgba(255, 255, 255, 0.8)',
-                borderRadius: '10px',
-                padding: '20px',
-                margin: '0 5px',
-              }}
-            >
-              <img
-                src={testimonial.image}
-                alt={`Testimonial ${testimonial.id}`}
-                style={{ width: '100%', height: 'auto', marginBottom: '10px' }}
-              />
-              <blockquote>{testimonial.quote}</blockquote>
-            </div>
-          ))}
+      <div style={{ 
+        width: '400px',
+        height: '400px', 
+        borderRadius: '10px',
+        background: "linear-gradient(to bottom right, #d63384, #6610f2)",
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+        <div style={{
+          padding: '20px',
+          backgroundColor: 'white',
+          borderRadius: '10px',
+          boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.5)',
+          maxWidth: '80%',
+          textAlign: 'center',
+        }}>
+          "This is a quote from someone. It can be a testimonial or a review of your product or service."
+        </div>
+      </div>
+      <div style={{ 
+        width: '400px', 
+        height: '400px', 
+        borderRadius: '10px',
+        background: "linear-gradient(to bottom right, #d63384, #6610f2)",
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+        <div style={{
+          padding: '20px',
+          backgroundColor: 'white',
+          borderRadius: '10px',
+          boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.5)',
+          maxWidth: '80%',
+          textAlign: 'center',
+        }}>
+          "This is another quote from someone. It can be a testimonial or a review of your product or service."
+        </div>
+      </div>
+      <div style={{ 
+        width: '400px', 
+        height: '400px', 
+        borderRadius: '10px',
+        background: "linear-gradient(to bottom right, #d63384, #6610f2)",
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+        <div style={{
+          padding: '20px',
+          backgroundColor: 'white',
+          borderRadius: '10px',
+          boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.5)',
+          maxWidth: '80%',
+          textAlign: 'center',
+        }}>
+          "This is a third quote from someone. It can be a testimonial or a review of your product or service."
         </div>
       </div>
     </div>
   );
 };
-
-
