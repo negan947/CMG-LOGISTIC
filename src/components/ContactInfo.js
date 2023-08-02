beffc5556888197e027a69ad189e32fa34ec1b89
negan947 <alexvb1@protonmail.com>
2023-08-02 11:33:18 +0300

    tes1

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -5,15 +5,17 @@ const ContactInfo = () => {
   return (
     <Col lg={12}>
       <div className="contact-bx wow slideInUp">
-        <Row>
-          <Col lg={6}>
-            <h3>Contact Us</h3>
-          </Col>
-          <Col lg={6} md={6} xl={5}>
-            <p className="contact-info"><span className="contact-label">Tel:</span> <span className="contact-value">0772125155</span></p>
-            <p className="contact-info"><span className="contact-label">Email:</span> <span className="contact-value">contact@cmg-logistic.com</span></p>
-          </Col>
-        </Row>
+        <div className="contact-heading">
+          <h3>Contact Us</h3>
+        </div>
+        <div className="contact-details">
+          <Row>
+            <Col lg={6} md={6} xl={5}>
+              <p className="contact-info"><span className="contact-label">Tel:</span> <span className="contact-value">+40 772 125 155</span></p>
+              <p className="contact-info"><span className="contact-label">Email:</span> <span className="contact-value">contact@cmg-logistic.com</span></p>
+            </Col>
+          </Row>
+        </div>
       </div>
     </Col>
   );
